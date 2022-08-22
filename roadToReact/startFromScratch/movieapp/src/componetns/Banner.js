59c988e77184af77bd8ef6145cf72c61358f5d0e
konain1 <neeruali99@gmail.com>
2022-08-22 18:14:59 +0530

    paginationUpdated

--- roadToReact/startFromScratch/movieapp/src/componetns/Banner.js
+++ roadToReact/startFromScratch/movieapp/src/componetns/Banner.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
 export class Banner extends Component {
+
   render() {
+    const poster  = '/6EdKBYkB1ssgGjc249ud1L55o8d.jpg'
     return (
     
       <div className="card banner-card" >
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt6C1dQRug0yAZhzjYwIQgQH2MfKh9exsObg&usqp=CAU" className="card-img-top" alt="..." />
+      <img src={`https://image.tmdb.org/t/p/original${poster}`} className="card-img-top" alt="..." />
      
         <h5 className="card-title banner-title">Card title</h5>
         <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
--- roadToReact/startFromScratch/movieapp/src/componetns/MoviesList.js
+++ roadToReact/startFromScratch/movieapp/src/componetns/MoviesList.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import {movies} from '../movieData'
 
 export class MoviesList extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      hover : '',
+      parr : [1]  
+    };
+  }
   render() {
 
     let moviesArr = movies.results
@@ -15,11 +24,19 @@ export class MoviesList extends Component {
 
       <div className='moviesList'>
       {moviesArr.map((ele)=>(
-        <div className="card movie-card">
+        <div className="card movie-card" onMouseEnter={()=> this.setState({hover: ele.id})}
+        onMouseLeave={()=>this.setState({hover:''})}>
+        
         <img src={`https://image.tmdb.org/t/p/original${ele.backdrop_path}`} className="card-img-top" alt="..."/>
         <h5 className="card-title " >{ele.title}</h5>
 
-        <div className='button-wrap' style={{display:'flex' , justifyContent:'center'}}> <a href="#" class="btn btn-primary movie-btn">Add to Favourite</a>
+        <div className='button-wrap' style={{display:'flex' , justifyContent:'center'}}> 
+        {
+          this.state.hover == ele.id && (
+            <a href="#" class="btn btn-primary movie-btn">Add to Favourite</a>
+          )
+        }
+        
         </div>
 
 
@@ -36,9 +53,12 @@ export class MoviesList extends Component {
       <nav aria-label="Page navigation example">
       <ul class="pagination">
         <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
+
+        {this.state.parr.map((value)=>(
+          <li class="page-item"><a class="page-link" href="#">{value}</a></li>
+        ))}
+       
+        
         <li class="page-item"><a class="page-link" href="#">Next</a></li>
       </ul>
     </nav>
