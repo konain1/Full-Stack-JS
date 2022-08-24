8ba5ed4bc536bde744210e7f8e82d6a70261de1c
konain1 <neeruali99@gmail.com>
2022-08-24 17:22:11 +0530

    nextprevPagevalue

--- roadToReact/startFromScratch/movieapp/src/componetns/MoviesList.js
+++ roadToReact/startFromScratch/movieapp/src/componetns/MoviesList.js
@@ -1,71 +1,140 @@
-import React, { Component } from 'react'
-import {movies} from '../movieData'
+import React, { Component } from "react";
+// import {movies} from '../movieData'
+import axios from "axios";
 
 export class MoviesList extends Component {
-
-  constructor(){
-    super()
+  constructor() {
+    super();
 
     this.state = {
-      hover : '',
-      parr : [1]  
+      hover: "",
+      parr: [1],
+      movies: [],
+      currPage :1
     };
   }
-  render() {
 
-    let moviesArr = movies.results
-    console.log(moviesArr)
+  async componentDidMount() {
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=eb29820ff34c7a78e2eed374a36d8824&language=en-US&page=${this.state.currPage}`
+    );
+    const apiMovieData = res.data;
+    console.log(apiMovieData);
+
+    this.setState({
+      movies: [...apiMovieData.results],
+    });
+  }
+  changeMovies = async () => {
+    const res = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=eb29820ff34c7a78e2eed374a36d8824&language=en-US&page=${this.state.currPage}`
+    );
+    const apiMovieData = res.data;
+    console.log(apiMovieData);
+
+    this.setState({
+      movies: [...apiMovieData.results],
+    });
+  };
+
+  handleNext = () => {
+    let tempArr = [];
+    for (let i = 1; i <= this.state.parr.length + 1; i++) {
+      tempArr.push(i);
+      
+    }
+    console.log(tempArr+"---->>>>");
+
+    this.setState({
+      parr : [...tempArr],
+     currPage:this.state.currPage+1,
+    
+      
+    },this.changeMovies)
+   
+    console.log(this.state.currPage)
+   
+  };
+
+  handleprev = ()=>{
+    if(this.state.currPage != 1){
+      this.setState({
+        currPage:this.state.currPage-1,
+        parr:this.state.currPage-1
+        
+      },this.changeMovies)
+
+  }}
+  handlePageValue = (value)=>{
+    if(value != this.state.currPage){
+      this.setState({
+        currPage:value
+      },this.changeMovies)
+    }
+  }
+  render() {
     return (
       <>
-
-      <div> 
-      <h3 className='text-center'>Trending</h3>
-      </div>
-
-      <div className='moviesList'>
-      {moviesArr.map((ele)=>(
-        <div className="card movie-card" onMouseEnter={()=> this.setState({hover: ele.id})}
-        onMouseLeave={()=>this.setState({hover:''})}>
-        
-        <img src={`https://image.tmdb.org/t/p/original${ele.backdrop_path}`} className="card-img-top" alt="..."/>
-        <h5 className="card-title " >{ele.title}</h5>
-
-        <div className='button-wrap' style={{display:'flex' , justifyContent:'center'}}> 
-        {
-          this.state.hover == ele.id && (
-            <a href="#" class="btn btn-primary movie-btn">Add to Favourite</a>
-          )
-        }
-        
+        <div>
+          <h3 className="text-center">Trending</h3>
         </div>
 
+        <div className="moviesList">
+          {this.state.movies.map((ele) => (
+            <div
+              className="card movie-card"
+              onMouseEnter={() => this.setState({ hover: ele.id })}
+              onMouseLeave={() => this.setState({ hover: "" })}
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/original${ele.backdrop_path}`}
+                className="card-img-top"
+                alt="..."
+              />
+              <h5 className="card-title ">{ele.title}</h5>
 
-       
-      </div>
-        
-      
-        
-      ))}
-      
-      </div>
+              <div
+                className="button-wrap"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
+                {this.state.hover == ele.id && (
+                  <a href="#" class="btn btn-primary movie-btn">
+                    Add to Favourite
+                  </a>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
 
-      <div style={{display:'flex' , justifyContent:'center'}}>
-      <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li class="page-item">
+                <a class="page-link"  onClick={this.handleprev}>
+                  Previous
+                </a>
+              </li>
 
-        {this.state.parr.map((value)=>(
-          <li class="page-item"><a class="page-link" href="#">{value}</a></li>
-        ))}
-       
-        
-        <li class="page-item"><a class="page-link" href="#">Next</a></li>
-      </ul>
-    </nav>
-    </div>
+              {this.state.parr.map((value) => (
+                <li class="page-item">
+                  <a class="page-link" onClick={()=>{this.handlePageValue(value)}} >
+                    {value}
+                  </a>
+                </li>
+              ))}
+
+              <li class="page-item">
+                <a class="page-link" onClick={this.handleNext}>
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </>
-    )
+    );
   }
 }
 
-export default MoviesList
+export default MoviesList;
