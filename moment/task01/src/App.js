65cca32133c7221e493065905930185151787d54
konain1 <neeruali99@gmail.com>
2022-09-12 18:19:36 +0530

    added

---
+++ moment/task01/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import CourseSlider from './components/CourseSlider';
+
+function App() {
+  return (
+    <div className="App">
+   <CourseSlider/>
+    </div>
+  );
+}
+
+export default App;
---
+++ moment/task01/src/components/CourseSlider.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class CourseSlider extends Component {
+  render() {
+    return (
+      <>
+       <div className='cont'>
+
+        <div className='cont__item'>1</div>
+        <div className='cont__item'>2</div>
+        <div className='cont__item'>3</div>
+
+        <div className='cont__item'>4</div>
+        <div className='cont__item'>5</div>
+        <div className='cont__item'>6</div>
+
+       </div>
+      </>
+    )
+  }
+}
+
+export default CourseSlider
