09177fc3469cfb068085c582999bb3dd64d82742
konain1 <neeruali99@gmail.com>
2022-09-15 18:15:32 +0530

    selectionSort

---
+++ OOps/sorting/selectionSort.js
@@ -0,0 +1,18 @@
+
+
+let arr = [5,1,3,4,2]
+
+// search smallest value and out on 0th index
+
+for(let i = 0;i<arr.length;i++){
+    let min = i;
+
+    for(let j = i+1;j<arr.length;j++){
+        if(arr[j] < arr[min]){
+            let temp = arr[j];
+            arr[j] = arr[min];
+            arr[min] = temp;
+         }
+    }
+}
+console.log(arr)
