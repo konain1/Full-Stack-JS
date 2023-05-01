4dad34fd376cd3dd13c6d4763984f7c8bf4679f7
konain1 <neeruali99@gmail.com>
2023-05-01 14:38:34 +0530

    1%done

---
+++ clients/coaching-website/enlighten/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Container from './pages/Container';
+function App() {
+  return (
+    <div className="App">
+     <Container/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ clients/coaching-website/enlighten/src/pages/Container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Nav from './Nav'
+function Container() {
+  return (
+    <div>
+      <Nav></Nav>
+    </div>
+  )
+}
+
+export default Container
