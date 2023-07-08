a1c70f61240cdb4967e8f92fe6403273cc2d593c
konain1 <neeruali99@gmail.com>
2023-07-09 02:17:40 +0530

    pagination with backend data DONE

---
+++ roadToReact/paginationwithBackednData/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Page from './Page'
+
+function App() {
+  return (
+    <div>
+    <h1>pagination</h1>
+      <Page></Page>
+    </div>
+  )
+}
+
+export default App
---
+++ roadToReact/paginationwithBackednData/src/Page.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Page.css';
+
+function Page() {
+  const [products, setProducts] = useState([]);
+  const [page,setPages] = useState(1)
+  const [totalPages,setTotalPages] = useState(0)
+
+  const url = `https://dummyjson.com/products?limit=12&skip=${page *12 -12}`;
+
+ 
+  
+
+  const getProducts = async () => {
+    const res = await axios.get(url).then((response) => response.data);
+    setProducts(res.products);
+    // console.log(res.total)
+    setTotalPages(res.total)
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const pageHandler = (pageIndex)=>{
+    if(pageIndex >=1 && pageIndex < totalPages / 10  && pageIndex  !== page){
+        setPages(pageIndex)
+    }
+
+  
+  }
+
+  return (
+    <div className="page">
+      <h1 className="page-title">Welcome to our Online Store</h1>
+      <div className="product-list">
+        {products.length > 0 &&
+          products.map((product, index) => (
+            <div className="product" key={index}>
+              <img className="product-image" src={product.thumbnail} alt={product.title} />
+              <h3 className="product-title">{product.title}</h3>
+              <p className="product-description">{product.description}</p>
+              <span className="product-price">${product.price}</span>
+              <button className="add-to-cart">Add to Cart</button>
+            </div>
+          ))}
+      </div>
+      <div className='pagination'>
+      <span className={page < totalPages /10 -1 ?"pageIndex":'pageDisable'} onClick={()=>{pageHandler(page + 1)}}>Next</span>
+          {[...Array(totalPages /10  )].slice(0,totalPages / 10 -1).map((_,pageIndex)=>{
+            return <span onClick={()=>pageHandler(pageIndex+1)} className={page === pageIndex+1 ? 'pageIndexHover':'pageIndex'} key={pageIndex}>{pageIndex + 1}</span>
+          })}
+          <span  className={page > 1 ?'pageIndex':'pageDisable'}  onClick={()=>{pageHandler(page - 1)}}>Previous</span>
+          </div>
+    </div>
+  );
+}
+
+export default Page;
