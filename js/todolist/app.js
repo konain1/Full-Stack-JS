c7e6b8d7d6830fa620d5de2980732cd7c163b426
konain1 <kaunainkasab@gmail.com>
2023-07-26 22:42:33 +0530

    todolist

---
+++ js/todolist/app.js
@@ -0,0 +1,34 @@
+
+
+const myInput = document.getElementById('input')
+const myDiv = document.getElementById('div')
+
+
+myInput.addEventListener('input',function(event){
+
+    
+    myInput.addEventListener('keypress',function(e){
+        if(e.keyCode === 13){
+            const taskText = myInput.value.trim();
+            if(taskText === '' ) return;
+            makeElements(taskText);
+        }
+    })
+
+})
+
+function makeElements(task){
+    const li = document.createElement('li');
+    li.classList.add('li');
+    const span = document.createElement('span')
+    const btn = document.createElement('button');
+    btn.textContent = 'x';
+    li.appendChild(btn);
+    span.textContent = task
+    li.insertAdjacentElement('afterbegin',span)
+    myDiv.appendChild(li)
+    myInput.value = ''
+   
+
+}
+
