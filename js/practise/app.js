c96ad8b0e83076a45758979d52e185f7ccaa3ed6
konain1 <kaunainkasab@gmail.com>
2023-07-29 13:57:04 +0530

    aewe ai

---
+++ js/practise/app.js
@@ -0,0 +1,13 @@
+
+
+
+
+const str = "Hello, world!";
+const newArray = Array.from(str);
+console.log(newArray);
+
+
+
+const numbers = [1,2,3,4]
+const doubledArray = Array.from(numbers*3);
+console.log(doubledArray)
